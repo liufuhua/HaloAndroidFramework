a5fe27540b4538bb5412ff45f51a84aa72f90667
刘福桦 <liufuhua007@gmail.com>
2016-09-16 23:44:24 +0800

    add tab view indicator module and demo code

--- app/src/main/java/com/halo/haloandroidframework/MainActivity.java
+++ app/src/main/java/com/halo/haloandroidframework/MainActivity.java
@@ -1,7 +1,11 @@
 package com.halo.haloandroidframework;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.halo.haloandroidframework.TabViewIndicatorExample.example.ExampleMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tab_view_indicator:
+                startActivity(new Intent(this, ExampleMainActivity.class));
+                break;
+            default:
+                break;
+        }
     }
 }
