b5f5ced6f06a6cb8d7da367dca7c7cb19d98547d
gitdahai <yangdahai@hollo.cn>
2016-09-22 09:54:44 +0800

    fix

--- commons/src/main/java/commons/GeneralTools.java
+++ commons/src/main/java/commons/GeneralTools.java
@@ -8,6 +8,11 @@ import android.util.DisplayMetrics;
  */
 public class GeneralTools {
 
+    /**
+     * 由字符串形式的十六进制转换成Integer类型
+     * @param hexColorString24bits
+     * @return
+     */
     public static int formatColorFromHexString(String hexColorString24bits){
         return 0xff000000 | Integer.decode(hexColorString24bits);
     }
