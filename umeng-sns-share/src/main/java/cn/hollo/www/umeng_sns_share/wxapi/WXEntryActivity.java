f9a958ec7f20083ea98078ba3a6eebfd759ac4b8
刘福桦 <liufuhua007@gmail.com>
2017-01-19 13:45:25 +0800

    添加sns-share  修改config配置 添加badge

---
+++ umeng-sns-share/src/main/java/cn/hollo/www/umeng_sns_share/wxapi/WXEntryActivity.java
@@ -0,0 +1,12 @@
+package cn.hollo.www.umeng_sns_share.wxapi;
+
+
+//import com.umeng.weixin.callback.WXCallbackActivity;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+public class WXEntryActivity extends WXCallbackActivity {
+
+
+
+}
